8f773920101fa810a7d330c74db298fe303413f0
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-03-13 10:46:30 +0900

    문법 (if) 설명 코드

--- app/src/main/java/com/neppplus/kotlinbasic2_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic2_20210313/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.kotlinbasic2_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,12 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+//        if문 연습 코드
+
+        val myAge = 25
+
+        if (myAge >= 30) {
+            Log.d("나이", "30대 이상")
+        }
+        else if (myAge >= 20) {
+            Log.d("나이", "20대")
+        }
+        else {
+            Log.d("나이", "미성년자")
+        }
+
         sendBtn.setOnClickListener {
 
 //            버튼이 눌리면
 //            1. inputEdt에 적힌 내용을 => inputMessage 변수에 메모. : get
 
-            val inputMessage = inputEdt.text.toString()
+            val inputMessage = inputEdt.text.toString() // val 변수 = 오른쪽 : String
+
+
+            val num1 = 20  // val 변수 = 오른쪽 : Int
 
 //            2. contentTxt의 text속성값을 => inputMessage에 적힌 값으로 변경. : set
 
